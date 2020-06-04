ed1412457c0af989b726d2d795095f31bdaa5b1d
Manase Mhando <manase.mhando@mindvalley.com>
2020-06-04 11:44:48 +0800

    [CHORE] Added scroll content view size

--- Say Their Names/Source/Controller/Home/Person/Views/PersonView.swift
+++ Say Their Names/Source/Controller/Home/Person/Views/PersonView.swift
@@ -11,9 +11,19 @@ import UIKit
 final class PersonView: UIView {
  
     lazy var scrollView: UIScrollView = {
-        let view = UIScrollView(frame: superview!.bounds)
+        let view = UIScrollView(frame: .zero)
         view.backgroundColor = .red
+        view.frame = self.superview!.bounds
+        view.contentSize = self.contentViewSize
+        view.autoresizingMask = .flexibleHeight
+        view.showsHorizontalScrollIndicator = true
+        view.bounces = true
+        view.translatesAutoresizingMaskIntoConstraints = false
         return view 
+    }()
+    
+    lazy var contentViewSize: CGSize = {
+        return CGSize(width: self.superview!.bounds.width, height: self.superview!.bounds.height + 400)
     }()
     
     lazy var personCarouselView: UIView = {
@@ -53,10 +63,13 @@ final class PersonView: UIView {
         return imageView
     }()
     
-    lazy var navigationControlView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .blue
-        return view
+    lazy var actionStackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [dismissButton, shareButton])
+        stack.axis = .horizontal
+        stack.alignment = .fill
+        stack.distribution = .equalSpacing
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
     }()
     
     let shareButton: UIButton = {
@@ -81,21 +94,13 @@ final class PersonView: UIView {
     
     
     private func createLayout() {
-        let scroll = UIScrollView(frame: superview!.bounds)
-        scroll.backgroundColor = .red
         
         personImageView.translatesAutoresizingMaskIntoConstraints = false
         personImageContainer.translatesAutoresizingMaskIntoConstraints = false
         
         dismissButton.translatesAutoresizingMaskIntoConstraints = false
         shareButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        let actionStackView = UIStackView(arrangedSubviews: [dismissButton, shareButton])
-        actionStackView.axis = .horizontal
-        actionStackView.alignment = .fill
-        actionStackView.distribution = .equalSpacing
-        actionStackView.translatesAutoresizingMaskIntoConstraints = false
-        
+                
         personImageContainer.addSubview(personImageView)
         visualEffectView.contentView.addSubview(actionStackView)
         
@@ -103,10 +108,15 @@ final class PersonView: UIView {
         personCarouselView.addSubview(bgPersonImageView)
         personCarouselView.addSubview(visualEffectView)
         
-        scroll.addSubview(personCarouselView)
-        addSubview(scroll)
+        scrollView.addSubview(personCarouselView)
+        addSubview(scrollView)
         
         NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: self.superview!.leadingAnchor, constant: 0),
+            scrollView.topAnchor.constraint(equalTo: self.superview!.topAnchor, constant: 0),
+            scrollView.trailingAnchor.constraint(equalTo: self.superview!.trailingAnchor, constant: 0),
+            scrollView.bottomAnchor.constraint(equalTo: self.superview!.bottomAnchor, constant: 0),
+            
             actionStackView.leadingAnchor.constraint(equalTo: visualEffectView.leadingAnchor, constant: 25),
             actionStackView.topAnchor.constraint(equalTo: visualEffectView.topAnchor, constant: 20),
             actionStackView.trailingAnchor.constraint(equalTo: visualEffectView.trailingAnchor, constant: -25),
